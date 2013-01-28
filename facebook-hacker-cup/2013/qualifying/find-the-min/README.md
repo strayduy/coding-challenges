da5e86a98046af56e9df39b771f5719a863da62f
Duy Tran <duy@strayharbor.com>
2013-01-28 01:24:19 -0800

    Naive solution for Find the Min problem. Still too slow to submit...

---
+++ facebook-hacker-cup/2013/qualifying/find-the-min/README.md
@@ -0,0 +1,4 @@
+Find the Min
+============
+
+[Problem description](https://www.facebook.com/hackercup/problems.php?pid=494433657264959&round=185564241586420)
---
+++ facebook-hacker-cup/2013/qualifying/find-the-min/find-the-min.py
@@ -0,0 +1,84 @@
+#!/usr/bin/env python2.7
+
+# Standard libs
+import collections
+import sys
+
+# Constants
+USAGE = "python %s INPUT_FILE" % (sys.argv[0])
+
+def main(argv):
+    if len(argv) != 2:
+        raise Exception("Invalid arguments! Usage: %s" % (USAGE))
+
+    input_filename = argv[1]
+
+    with open(input_filename, "r") as input_file:
+        # Read in the number of test cases
+        try:
+            num_test_cases = int(input_file.readline().strip())
+        except:
+            raise Exception("Invalid input file")
+
+        # Generate the m array for each test case
+        # Compute the nth element of each m array
+        for test_case_number in range(1, num_test_cases + 1):
+            first_line = input_file.readline().strip()
+            second_line = input_file.readline().strip()
+
+            n, k = map(int, first_line.split(" "))
+            a, b, c, r = map(int, second_line.split(" "))
+
+            m = generate_m(k, a, b, c, r)
+            nth_element = get_nth_element_of_m(m, n)
+            print "Case #%d: %d" % (test_case_number, nth_element)
+
+# Generator that spits out successive values for an m array
+def next_min(initial_values):
+    # sliding_window keeps track of the previous k elements.
+    # deque has constant time appending and removal of elements from the ends
+    # of the list.
+    sliding_window = collections.deque(initial_values)
+
+    # occurrences keeps track of how many times each value appears in the list
+    # Counter has constant time "contains" lookup
+    occurrences = collections.Counter(initial_values)
+
+    while True:
+        # Increment upwards from zero until we find a value that doesn't
+        # already appear in our list
+        next_value = 0
+        while next_value in occurrences:
+            next_value += 1
+        yield next_value
+
+        # Pop off the leftmost value from the list
+        # Update our counter accordingly
+        popped_value = sliding_window.popleft()
+        occurrences[popped_value] -= 1
+        if occurrences[popped_value] <= 0:
+            del occurrences[popped_value]
+
+        # Append the new value to the end (right) of the list
+        # Update our counter accordingly
+        sliding_window.append(next_value)
+        occurrences[next_value] += 1
+
+def get_nth_element_of_m(m, n):
+    limit = n - len(m)
+
+    # Iterate through our generator until we get to the nth element
+    for i, value in enumerate(next_min(m)):
+        if i >= limit - 1:
+            break
+
+    return value
+
+def generate_m(k, a, b, c, r):
+    m = [a]
+    for i in range(1, k):
+        m.append((b * m[i - 1] + c) % r)
+    return m
+
+if __name__ == "__main__":
+    main(sys.argv)
